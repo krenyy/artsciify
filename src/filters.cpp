052ceb91913aa411aa253ebd7e8ec24db8bbabaf
Martin Kröner <kronemar@fit.cvut.cz>
2023-04-24 00:38:27 +0200

    add grayscale filter

--- src/filters.cpp
+++ src/filters.cpp
@@ -1,6 +1,17 @@
 #include "filters.h"
 #include "image.h"
 
+void Grayscale::apply(Image &img) const {
+  size_t height = img[0].size(), width = img[0][0].size();
+  for (size_t i = 0; i < height; ++i) {
+    for (size_t j = 0; j < width; ++j) {
+      double px_brightness =
+          0.2126 * img[0][i][j] + 0.7152 * img[1][i][j] + 0.0722 * img[2][i][j];
+      img[0][i][j] = img[1][i][j] = img[2][i][j] = px_brightness;
+    }
+  }
+}
+
 void Negative::apply(Image &img) const {
   for (auto &channel : img) {
     for (auto &row : channel) {
--- src/filters.h
+++ src/filters.h
@@ -5,6 +5,11 @@ public:
   virtual void apply(Image &img) const = 0;
 };
 
+class Grayscale : public Filter {
+public:
+  void apply(Image &img) const override;
+};
+
 class Negative : public Filter {
 public:
   void apply(Image &img) const override;
