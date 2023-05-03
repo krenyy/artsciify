c053154ef19f6173995c17b3ad5c8ecf41375065
Martin Kröner <kronemar@fit.cvut.cz>
2023-05-03 13:42:40 +0200

    image: create a Pixel struct

--- src/image.h
+++ src/image.h
@@ -1,9 +1,22 @@
 #ifndef ARTSCIIFY_IMAGE_H
 #define ARTSCIIFY_IMAGE_H
 
+#include <array>
 #include <optional>
 #include <png.h>
 #include <vector>
+
+struct Pixel {
+  std::array<unsigned char, 3> rgb;
+  unsigned char &r() { return rgb[0]; }
+  unsigned char &g() { return rgb[1]; }
+  unsigned char &b() { return rgb[2]; }
+  unsigned char r() const { return rgb[0]; }
+  unsigned char g() const { return rgb[1]; }
+  unsigned char b() const { return rgb[2]; }
+  unsigned char &operator[](const size_t i) { return rgb[i]; }
+  unsigned char operator[](const size_t i) const { return rgb[i]; }
+};
 
 using ImagePixel = png_byte;
 using ImageRow = std::vector<ImagePixel>;
