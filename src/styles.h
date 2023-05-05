6ba4e412a2f6820822046784517d6bee62b800c8
Martin Kröner <kronemar@fit.cvut.cz>
2023-05-05 17:19:53 +0200

    styles: get rid of configurable gradients

--- src/styles.h
+++ src/styles.h
@@ -30,8 +30,6 @@ public:
   class Map : public std::map<double, unsigned char> {
   public:
     static Map build(std::string characters, std::vector<double> brightnesses);
-    static Map standard();
-    static Map eddie_smith();
   };
 
   AsciiTextTransform(const double brightness_r, const double brightness_g,
@@ -77,12 +75,6 @@ class ArtStyle {
 public:
   ArtStyle(std::shared_ptr<TextTransform> text_transform,
            std::vector<std::shared_ptr<ColorTransform>> color_transforms);
-  static ArtStyle ascii_standard();
-  static ArtStyle ascii_standard_color();
-  static ArtStyle ascii_eddie_smith();
-  static ArtStyle ascii_eddie_smith_color();
-  static ArtStyle character(const unsigned char c);
-  static ArtStyle block();
   std::string print(const Image &img) const;
 
 private:
