b2a85391b992e7269d0e34c7b438dd10d4abbcf6
Martin Kröner <kronemar@fit.cvut.cz>
2023-05-13 23:13:23 +0200

    presentation: documentation

--- src/presentation.h
+++ src/presentation.h
@@ -8,9 +8,21 @@
 #include <queue>
 #include <vector>
 
+/// Represents a presentation in the terminal.
+///
+/// Handles loading image files, storing the state, carrying
+/// out various operations, and also writing.
 class Presentation {
 public:
+  /// Initialize a new Presentation instance with the provided config and image
+  /// paths.
+  ///
+  /// \param cfg Config object constructed by reading a config file
+  /// \param image_paths Image paths to load and do operations on
   Presentation(Config cfg, std::vector<std::filesystem::path> image_paths);
+  /// Initiates the presentation
+  ///
+  /// Uses stderr for output
   void start();
 
 private:
@@ -41,6 +53,16 @@ private:
   void handle_write_file();
   void handle_write_to_stream(std::ostream &os);
   void handle_quit() const;
+
+  /// Reads input from stdin until a newline.
+  ///
+  /// Skips control characters.
+  ///
+  /// \throws std::runtime_error on EOF
+  /// \returns std::string containing the retrieved input
   std::string read_input() const;
+  /// Reads an integer from stdin using the read_input method
+  ///
+  /// \returns An integer. If the conversion failed, returns std::nullopt
   std::optional<long> read_integer() const;
 };
